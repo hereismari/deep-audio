7f243ec8b86a614ab5bf556b7cd2f8770f284e39
Marianne Linhares Monteiro <mariannelinharesm@gmail.com>
2018-08-26 11:25:54 -0300

    Downloading data from youtube

--- src/download_data/get_data_from_youtube.py
+++ src/download_data/get_data_from_youtube.py
@@ -1,9 +1,19 @@
+'''
+Author: Marianne Linhares, mariannelinharesm@gmail.com
+Last Change: 08/2018
+
+Based on the script from BGodefroyFR
+at: https://github.com/BGodefroyFR/Deep-Audio-Visualization/blob/master/learning/scripts/downloadTrainingSamples.py
+
+'''
+
+
 from __future__ import unicode_literals
-import commands
 import argparse
 
 import utilities.utils as utils
 import numpy as np
+import os
 
 
 parser = argparse.ArgumentParser('')
@@ -19,40 +29,34 @@ parser.add_argument('--seed', type=int, default=7)
 parser.add_argument('--youtube-conf', type=str, default='download_data/conf/youtube.json')
 
 
-def download_youtube_audio(output_path, playlist_file):
-    # Loads laylists
-    with open("./playlists.txt", "r") as f:
-        playlists = []
-        for line in f:
-            playlists.append(line)
-        playlists = [x.strip() for x in playlists] 
-
-
-    for i in range(0, int(nbVideos)):
-        print("Download " + repr(i))
-        try:
-            playlistInd = randint(1, 500)
-            commands.getstatusoutput("youtube-dl \
-                --ignore-errors \
-                --audio-quality 0 \
-                -x \
-                -o '" + os.path.join(output_path, + repr(i) + ".%(ext)s' \
-                --audio-format wav \
-                --playlist-start " + repr(playlistInd) + " --playlist-end " + repr(playlistInd) + " "\
-                + playlists[i % len(playlists)]))
-        except:
-            print("Error during download")
-
-
-
-def download_from_playlist_file(playlist_file, songs_per_laylist, output_path):
+def download_from_playlist_file(playlist_file, songs_per_playlist, output_path):
     playlists_urls = utils.load_json(playlist_file)
     for playlist in playlists_urls:
-        id = utils.id_from_youtube_url(playlists_urls[playlist])
-        indices = np.random.choice(range(songs_per_laylist)), replace=False)
-        
+        playlist_id = utils.id_from_youtube_url(playlists_urls[playlist]['url'])
+        length = playlists_urls[playlist]['length']
+        indices = np.random.choice(range(length), size=min(length, songs_per_playlist), replace=False)
 
+        for index in indices:
+            print("Downloading song %s from playlist %s" % (index, playlist))
+            final_filename = os.path.join(output_path,  '%s_%s' % (playlist, index))
+            output_filename = '"' + final_filename + '.%(ext)s"'
+            utils.mkdir_if_needed(os.path.dirname(output_filename))
 
+            if os.path.exists(final_filename + '.wav'):
+                continue
+
+            try:
+                command = ('youtube-dl \
+                    --extract-audio \
+                    --ignore-errors \
+                    --audio-quality 0 \
+                    -x \
+                    -o ' + output_filename + ' --audio-format wav \
+                    --playlist-start ' + str(index) + ' --playlist-end ' + str(index) + ' ' \
+                    + playlist_id)
+                utils.get_status_output(command)
+            except:
+                print("Error during download :(")
 
 
 def download_from_playlist_list():
@@ -68,7 +72,7 @@ if __name__ == "__main__":
     np.random.seed(args.seed)
     
     if args.run_mode == 'playlist-file':
-        download_from_playlist_file()
+        download_from_playlist_file(args.playlist_file, args.songs_per_playlist, args.output_path)
     elif args.run_mode == 'playlist-list':
         download_from_playlist_list()
     elif args.run_mode == 'video-list':
--- src/download_data/utilities/utils.py
+++ src/download_data/utilities/utils.py
@@ -1,5 +1,8 @@
 import regex
 import json
+import os
+import subprocess
+
 
 def id_from_youtube_url(url):
     match = regex.match('^(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?.*?(?:v|list)=(.*?)(?:&|$)|^(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?(?:(?!=).)*\/(.*)$',
@@ -13,3 +16,14 @@ def id_from_youtube_url(url):
 def load_json(filename):
     f = open(filename)
     return json.load(f)
+
+
+def mkdir_if_needed(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+
+def get_status_output(command):
+    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+    p.wait()
+    return p.returncode
