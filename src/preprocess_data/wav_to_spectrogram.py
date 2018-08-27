972674d297aeec340fbb95efc4c736b69ed09160
Marianne Linhares Monteiro <mariannelinharesm@gmail.com>
2018-08-26 22:53:58 -0300

    Adding basic data preprocessing

--- src/preprocess_data/wav_to_spectrogram.py
+++ src/preprocess_data/wav_to_spectrogram.py
@@ -5,26 +5,24 @@ Last Change: 08/2018
 Based on the script from BGodefroyFR
 at: https://github.com/BGodefroyFR/Deep-Audio-Visualization/blob/master/learning/scripts/downloadTrainingSamples.py
 '''
-
-from __future__ import unicode_literals
 import argparse
 
-import utilities.utils as utils
+import matplotlib.pyplot as plt
+import librosa
+import librosa.display
+
 import numpy as np
 import os
 
 
 parser = argparse.ArgumentParser('')
 
-parser.add_argument('--run-mode', type=str, default='playlist-file',
-                    choices=['playlist-file', 'video-list', 'playlist-list'])
-parser.add_argument('--output-path', type=str, default='audio_files/')
+parser.add_argument('--run-mode', type=str, default='file',
+                    choices=['file', 'path'])
 
-parser.add_argument('--playlist-file', type=str, default='download_data/conf/playlists.json')
-parser.add_argument('--songs-per-playlist', type=int, default=10)
-parser.add_argument('--seed', type=int, default=7)
-
-parser.add_argument('--youtube-conf', type=str, default='download_data/conf/youtube.json')
+parser.add_argument('--output-path', type=str, default='preprocessed_files/')
+parser.add_argument('--input-path', type=str, default='audio_files/classical_145.wav')
+parser.add_argument('--window-size', type=int, default=11025)
 
 
 def download_from_playlist_file(playlist_file, songs_per_playlist, output_path):
@@ -56,22 +54,32 @@ def download_from_playlist_file(playlist_file, songs_per_playlist, output_path):
             except:
                 print("Error during download :(")
 
+def wav_to_spectrogram(input_path, output_path, window_size):
+    y, sr = librosa.load(input_path)
+    number_of_windows = int(len(y)/window_size)
+    spectrogram = np.empty((number_of_windows, window_size))
 
-def download_from_playlist_list():
-    pass
-
-
-def download_from_video_list():
-    pass
+    for i in range(number_of_windows):
+        spectrogram[i, :] = abs(np.fft.fft(y[i * window_size: (i+1) * window_size]))
+    
+    melgram = librosa.power_to_db(librosa.feature.melspectrogram(y, sr=sr, n_mels=128))
+    # melgram = melgram / np.linalg.norm(melgram)
+    
+    plt.figure(figsize=(10, 4))
+    librosa.display.specshow(melgram,
+                             y_axis='mel', fmax=8000,
+                             x_axis='time')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Mel spectrogram')
+    plt.tight_layout()
+    plt.show(), plt.pause(10)
+    return melgram
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    np.random.seed(args.seed)
     
-    if args.run_mode == 'playlist-file':
-        download_from_playlist_file(args.playlist_file, args.songs_per_playlist, args.output_path)
-    elif args.run_mode == 'playlist-list':
+    if args.run_mode == 'file':
+        wav_to_spectrogram(args.input_path, args.output_path, args.window_size)
+    elif args.run_mode == 'path':
         download_from_playlist_list()
-    elif args.run_mode == 'video-list':
-        download_from_video_list()
